b62c2830fef9a757648c684c3eda6d76993b46d8
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-28 08:45:10 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.getElementById('text')
+    message.innerHTML = "This is really cool!"
+})
